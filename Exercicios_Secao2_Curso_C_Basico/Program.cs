516098f559cadbf88f0f72c2af84dae0825ed246
rafael chaves <rafaelaugustochaves@hotmail.com>
2022-02-14 18:01:16 -0300

    Exercicio resolvido utilizando entrada e saidad de dados como tambem a aquisisao do ano atual

--- Exercicios_Secao2_Curso_C_Basico/Program.cs
+++ Exercicios_Secao2_Curso_C_Basico/Program.cs
@@ -6,7 +6,19 @@ namespace Exercicios_Secao2_Curso_C_Basico
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //programa para solicitar o nome de usuario e a idade.
+
+            Console.WriteLine("Olá, Digite seu nome:");
+            string nomeUsuario = Console.ReadLine();
+
+            Console.WriteLine(nomeUsuario + ", Digite sua idade:");
+            int idade = Convert.ToInt32(Console.ReadLine());
+            
+            int anoNascimentoAproximado = DateTime.Now.Year - idade;
+            
+            Console.WriteLine(nomeUsuario + ", o ano aproximado do seu nascimento é: " + anoNascimentoAproximado);
+
+            Console.ReadKey();
         }
     }
 }
