be78c3ae87ee72cb831ca7625612a49206a165dd
rafael chaves <rafaelaugustochaves@hotmail.com>
2022-02-14 17:47:24 -0300

    Adicionar arquivos de projeto.

---
+++ Exercicios_Secao2_Curso_C_Basico/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Exercicios_Secao2_Curso_C_Basico
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
